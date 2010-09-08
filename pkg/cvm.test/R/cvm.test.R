57d0839a9149a64761d6811e1e842f74650f8853
jayemerson (Dr ) <jayemerson@gmail.com>
2010-09-08 14:55:25 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ks-test@9 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/cvm.test/R/cvm.test.R
+++ pkg/cvm.test/R/cvm.test.R
@@ -1,7 +1,9 @@
-cvm.test <-
-function(x,y, type=c("W2", "U2", "A2")) {
+cvm.test <- function(x,y, type=c("W2", "U2", "A2")) {
+
   cvm.pval <- function(STAT, lambda) {
+
     x <- STAT
+
     theta <- function(u) {
       VAL <- 0
       for(i in 1:length(lambda)) {
@@ -20,15 +22,16 @@ function(x,y, type=c("W2", "U2", "A2")) {
     }
 
     fun <- function(u) return(sin(theta(u))/(u*rho(u)))
+
     pval <- 0
-    try(pval <- 0.5 + integrate(fun, 0, Inf, subdivisions=1e6)$value/pi, silent=TRUE)
+    try(pval <- 0.5 + integrate(fun, 0, Inf, subdivisions=1e6)$value/pi,
+        silent=TRUE)
     if(pval > 0.001) return(pval)
     if(pval <= 0.001) {
       df <- sum(lambda != 0)
       return(dchisq(STAT/max(lambda),df))
     }
-  }
-
+  } # End cvm.pval()
 
   cvm.stat <- function(x,y, type=c("W2", "U2", "A2")) {
     type <- match.arg(type)
@@ -60,24 +63,30 @@ function(x,y, type=c("W2", "U2", "A2")) {
     K <- diag(0, length(H))
     diag(K)[-length(H)] <- 1/(H[-length(H)]*(1-H[-length(H)]))
     Sy <- A %*% S0 %*% t(A)
-    M <- switch(type, W2 = E, U2 = (diag(1, nrow(E))-E%*%One%*%t(One))%*%E%*%(diag(1, nrow(E))-One%*%t(One)%*%E) , A2 = E%*%K)
+    M <- switch(type, W2 = E,
+                U2 = (diag(1, nrow(E)) - 
+                      E%*%One%*%t(One))%*%E%*%(diag(1, nrow(E)) -
+                      One%*%t(One)%*%E),
+                A2 = E%*%K)
     lambda <- eigen(M%*%Sy)$values
 
     STAT <- switch(type, W2 = sum(Z^2*t)/N, U2 = sum((Z-Zbar)^2*t)/N,
-                        A2 = sum((Z^2*t/(H*(1-H) ))[-length(I)])/N)
+                   A2 = sum((Z^2*t/(H*(1-H) ))[-length(I)])/N)
 
     return(c(STAT, lambda))
-  }
+  } # End cvm.stat()
 
 
   type <- match.arg(type)
   DNAME <- deparse(substitute(x))
-  if(length(setdiff(x, knots(y))) != 0) stop("Data are incompatable with null distribution")
+  if(length(setdiff(x, knots(y))) != 0) {
+    stop("Data are incompatable with null distribution")
+  }
   tempout <- cvm.stat(x,y,type=type)
   STAT <- tempout[1]
   lambda <- tempout[2:length(tempout)]
   PVAL <- cvm.pval(STAT, lambda)
-  METHOD <- paste("Cramer von Mises -", type)
+  METHOD <- paste("Cramer-von Mises -", type)
   names(STAT) <- as.character(type)
   RVAL <- list(statistic = STAT, p.value = PVAL, alternative = "Two.sided",
               method = METHOD, data.name=DNAME)
