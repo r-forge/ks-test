f5a9f099c600c08a092e38268982495a4bafc877
taylor3 (Mr. ) <taylor.b.arnold@gmail.com>
2010-11-09 16:47:13 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ks-test@24 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- rjournal/codeforgraphics.R
+++ rjournal/codeforgraphics.R
@@ -1,5 +1,44 @@
 #setwd('/home/tba3/Desktop/today/ks-test/JSS')
 
+  cvm.stat.disc <- function(x,y, type=c("W2", "U2", "A2")) {
+    type <- match.arg(type)
+    I <- knots(y)
+    N <- length(x)
+    e <- diff(c(0,N*y(I)))
+    obs <- rep(0, length(I))
+    for(j in 1:length(I)) {
+      obs[j] <- length(which(x == I[j]))
+    }
+    S <- cumsum(obs)
+    T <- cumsum(e)
+    H <- T/N
+    p <- e/N
+    t <- (p + p[c(2:length(p), 1)])/2
+    Z <- S - T
+    Zbar <- sum(Z*t)
+
+    S0 <- diag(p) - p %*% t(p)
+    A <- matrix(1, length(p), length(p))
+    A <- apply(row(A) >= col(A),2, as.numeric)
+    E <- diag(t)
+    One <- rep(1, nrow(E))
+    K <- diag(0, length(H))
+    diag(K)[-length(H)] <- 1/(H[-length(H)]*(1-H[-length(H)]))
+    Sy <- A %*% S0 %*% t(A)
+    M <- switch(type, W2 = E,
+                U2 = (diag(1, nrow(E)) - 
+                      E%*%One%*%t(One))%*%E%*%(diag(1, nrow(E)) -
+                      One%*%t(One)%*%E),
+                A2 = E%*%K)
+    lambda <- eigen(M%*%Sy)$values
+
+    STAT <- switch(type, W2 = sum(Z^2*t)/N, U2 = sum((Z-Zbar)^2*t)/N,
+                   A2 = sum((Z^2*t/(H*(1-H) ))[-length(I)])/N)
+
+    return(c(STAT, lambda))
+  } # End cvm.stat()
+
+
   cvm.pval <- function(STAT, lambda, good=TRUE) {
 
     x <- STAT
@@ -34,10 +73,13 @@
   } # End cvm.pval()
 
 #lambda <- sort(runif(10))
-lambda <- sort(c(0.020837 , 0.342575 , 0.531838 , 0.604825 , 0.645453 , 0.706874 , 0.750545 , 0.776914 , 0.810034 , 0.880271))
+#lambda <- sort(c(0.020837 , 0.342575 , 0.531838 , 0.604825 , 0.645453 , 0.706874 , 0.750545 , 0.776914 , 0.810034 , 0.880271))
+y <- ecdf(1:4)
+x <- sample(1:4)
+lambda <- cvm.stat.disc(x,y,'W2')[-1]
 cvm.pval(1, lambda)
 
-I <- seq(0,35,length.out=1000)
+I <- seq(0,5,length.out=500)
 y <- rep(NA, length(I))
 for(i in 1:length(I)) {
   y[i] <- cvm.pval(I[i], lambda, good=TRUE)
@@ -47,12 +89,14 @@ for(i in 1:length(I)) {
   z[i] <- cvm.pval(I[i], lambda, good=FALSE)
 }
 
-pdf('fig1.pdf', height=6, width=8)
-plot(I[I > 18], z[I > 18], type='l', xlab='test statistic', ylab='p-value', col='salmon', lwd=2)
+#pdf('fig1.pdf', height=6, width=8)
+coff <- 1.2
+coff2 <- 1.2
+plot(I[I > coff], z[I > coff], type='l', xlab='test statistic', ylab='p-value', col='salmon', lwd=2)
 abline(h=0, lty=2)
-lines(I[I > 21], y[I > 21], lty=2, col='blue', lwd=2)
-lines(I[I > 18], z[I > 18], col='salmon', lwd=2)
-dev.off()
+lines(I[I > coff2], y[I > coff2], lty=2, col='blue', lwd=2)
+lines(I[I > coff], z[I > coff], col='salmon', lwd=2)
+#dev.off()
 
 
 
